97ae592325787e0053736c40257b905fcc9d9a80
locas521 <locas521@outlook.com>
2016-03-29 13:57:11 +0800

    按照界面功能分包

--- app/src/main/java/com/example/vdllo/mirror/home/MainActivity.java
+++ app/src/main/java/com/example/vdllo/mirror/home/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.vdllo.mirror;
+package com.example.vdllo.mirror.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.vdllo.mirror.R;
 
 public class MainActivity extends AppCompatActivity {
 
