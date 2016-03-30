ad14cced747bcb13ffb230f14aea7b2ec7cdbb50
刘博 <317154369@qq.com>
2016-03-30 19:16:39 +0800

    no message

--- app/src/main/java/com/example/vdllo/mirror/home/AllTypeFragment.java
+++ app/src/main/java/com/example/vdllo/mirror/home/AllTypeFragment.java
@@ -1,17 +1,9 @@
 package com.example.vdllo.mirror.home;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.vdllo.mirror.R;
 import com.example.vdllo.mirror.base.BaseFragment;
-
 import java.util.ArrayList;
 import java.util.List;
 
