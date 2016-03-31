cb9bf1858c1a243ad3893b58fac3d6fc5ca82cff
胡乃鑫 <locas521@outlook.com>
2016-03-31 15:57:16 +0800

    no message

--- app/src/main/java/com/example/vdllo/mirror/base/BaseApplication.java
+++ app/src/main/java/com/example/vdllo/mirror/base/BaseApplication.java
@@ -3,6 +3,8 @@ package com.example.vdllo.mirror.base;
 import android.app.Application;
 import android.content.Context;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 /**
  * Created by Bo on 16/3/29.
  */
@@ -14,7 +16,7 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = this;
-//        Fresco.initialize(context);
+        Fresco.initialize(context);
 //        JPushInterface.setDebugMode(true);
 //        JPushInterface.init(this);
     }
