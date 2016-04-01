b414ca9fbc86d5b4c10d8ace68e1ff20dd99a516
胡乃鑫 <locas521@outlook.com>
2016-04-01 10:10:17 +0800

    no message

--- app/src/main/java/com/example/vdllo/mirror/home/AllTypeFragment.java
+++ app/src/main/java/com/example/vdllo/mirror/home/AllTypeFragment.java
@@ -81,6 +81,7 @@ public class AllTypeFragment extends BaseFragment {
         handler = new Handler(new Handler.Callback() {
             @Override
             public boolean handleMessage(Message msg) {
+                //1.Gson解析
                 Gson gson = new Gson();
                 goodsListBean = gson.fromJson(msg.obj.toString(), GoodsListBean.class);
                 // 2.设置布局管理器
@@ -107,6 +108,7 @@ public class AllTypeFragment extends BaseFragment {
                 .addParams("version", "").build().execute(new Callback() {
             @Override
             public Object parseNetworkResponse(Response response) throws Exception {
+                //子线程无法刷新UI,利用handler发送Message到主线程
                 String body = response.body().string();
                 Message message = new Message();
                 message.obj = body;
