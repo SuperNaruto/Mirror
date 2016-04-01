110e054a7d07ecbd99cc42861af26d43a6788e1c
胡乃鑫 <locas521@outlook.com>
2016-04-01 11:11:09 +0800

    no message

---
+++ app/src/main/java/com/example/vdllo/mirror/welcome/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.vdllo.mirror.welcome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/4/1.
+ */
+public class WelcomeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
