bf7fb5050b16d37748bc86ffe7f1e98f777a3d40
刘晓斐 <353193036@qq.com>
2016-04-16 15:21:22 +0800

    no message

--- app/src/main/java/com/example/vdllo/mirror/details/UpListViewAdapter.java
+++ app/src/main/java/com/example/vdllo/mirror/details/UpListViewAdapter.java
@@ -12,9 +12,6 @@ import com.example.vdllo.mirror.R;
 import com.example.vdllo.mirror.bean.GoodsListBean;
 import com.squareup.picasso.Picasso;
 
-import cn.sharesdk.framework.ShareSDK;
-import cn.sharesdk.onekeyshare.OnekeyShare;
-
 /**
  * Created by Bo on 16/4/8.
  */
@@ -86,37 +83,6 @@ public class UpListViewAdapter extends BaseAdapter {
                 listViewHeadHolder.detailPrice.setText(allGoodsListData.getData().getList().get(myPosition).getGoods_price());
                 listViewHeadHolder.detailTitle.setText(allGoodsListData.getData().getList().get(myPosition).getBrand());
                 listViewHeadHolder.detailBrand.setText(allGoodsListData.getData().getList().get(myPosition).getGoods_name());
-                listViewHeadHolder.shareIv.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        ShareSDK.initSDK(context);
-                        OnekeyShare oks = new OnekeyShare();
-                        //关闭sso授权
-                        oks.disableSSOWhenAuthorize();
-
-// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
-                        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
-                        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-                        oks.setTitle(context.getString(R.string.app_name));
-                        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-                        oks.setTitleUrl("http://sharesdk.cn");
-                        // text是分享文本，所有平台都需要这个字段
-                        oks.setText("我是分享文本");
-                        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-                        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
-                        // url仅在微信（包括好友和朋友圈）中使用
-                        oks.setUrl("http://sharesdk.cn");
-                        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-                        oks.setComment("我是测试评论文本");
-                        // site是分享此内容的网站名称，仅在QQ空间使用
-                        oks.setSite(context.getString(R.string.app_name));
-                        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-                        oks.setSiteUrl("http://sharesdk.cn");
-
-// 启动分享GUI
-                        oks.show(context);
-                    }
-                });
                 break;
             case TYPE_3:
                 convertView = inflater.inflate(R.layout.details_line_item, parent, false);
@@ -147,13 +113,12 @@ public class UpListViewAdapter extends BaseAdapter {
 
     public class ListViewHeadHolder {
         private TextView detailBrand, detailTitle, detailContext, detailPrice;
-        private ImageView shareIv;
+
         public ListViewHeadHolder(View view) {
             detailBrand = (TextView) view.findViewById(R.id.detail_head_brand);
             detailTitle = (TextView) view.findViewById(R.id.detail_head_title);
             detailContext = (TextView) view.findViewById(R.id.detail_head_context);
             detailPrice = (TextView) view.findViewById(R.id.detail_head_price);
-            shareIv = (ImageView) view.findViewById(R.id.details_head_share_iv);
         }
 
     }
